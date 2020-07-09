59dd04a98961f09031657a0a887c061ee0174c72
Apollo9999 <skjkiran0212@gmail.com>
2020-07-09 21:49:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 To Import data:
 
-mongoimport --db fashion --collection zappos-us_product --file /Users/kiran/Downloads/fashion/zappos-us_product.json
+mongoimport --db fashion --collection zappos-us_product --file / https://drive.google.com/file/d/1Tt2W5O6gFtUt6Q8MO7Eo9k33yjCc3K4X/view?usp=sharing/fashion/zappos-us_product.json
 
 To export data in required format :
 
@@ -9,7 +9,7 @@ mongoexport --db fashion --collection zappos-us_product --fields "meta,title,bra
  Solr command to upload data :
 ./solr start -e
 ./solr start -e cloud
-./post -c zappos-us_product -p 7575 /Users/kiran/zappos-us_product_cleanData2.json
+./post -c zappos-us_product -p 7575 /https://drive.google.com/file/d/1Tt2W5O6gFtUt6Q8MO7Eo9k33yjCc3K4X/view?usp=sharing/fashion/zappos-us_product_cleanData2.json
 
 Solr Query example :
 http://10.105.24.217:7574/solr/zappos-us_product/select?q=(((title%3Ablack%20)and%20(title%3A%20shirt))%5E%3D5)%20or%20(((description%20%3A%20black)%20and%20(description%20%3A%20shirt%20))%5E%3D4)%20or%20((brand%3ARobert%20Graham)%5E%3D2)%20or%20((gender%20%3A%20men)%20or%20(color_slug%20%3A%20black))%5E%3D6&rows=10&sort=score%20desc
